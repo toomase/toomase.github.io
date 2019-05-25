af0336c613d2d99d8d4fe8b779cef0509d11a62d
toomase <toomas.eilat@gmail.com>
2019-05-25 23:22:59 +0300

    lisa viide uhuele R versioonile

--- _drafts/r2jekyll.R
+++ _drafts/r2jekyll.R
@@ -4,7 +4,7 @@
 #.libPaths("C:/Users/toomase/Documents/R/win-library/3.3")
 
 ## R serverist käivitades path
-.libPaths("/home/rstudio/R/x86_64-pc-linux-gnu-library/3.5/")
+.libPaths("/home/rstudio/R/x86_64-pc-linux-gnu-library/3.6/")
 library(knitr)
 
 # Get the filename given as an argument in the shell.
